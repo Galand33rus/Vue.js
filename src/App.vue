c22e86d57e1fefa6b225f6c40ed3624001879df2
Galand33rus <galand_@mail.ru>
2021-09-27 08:04:04 +0300

    lesson3

---
+++ src/App.vue
@@ -0,0 +1,115 @@
+<template>
+  <div id="app">
+    <h2>My personal costs</h2>
+    <button class="new-cost" @click="showForm=!showForm">add new cost <span v-if="!showForm">&#8595;</span><span v-if="showForm">&#8593;</span></button>
+    <CostsAdd v-if="showForm" @addCostsItem="addCostsItem"/>
+    <CostsList :items="paymentsList"/>
+  </div>
+</template>
+
+<script>
+import CostsList from './components/CostsList.vue'
+import CostsAdd from './components/CostsAdd.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CostsList,
+    CostsAdd
+  },
+  data () {
+    return {
+      paymentsList: [],
+      showForm: false
+    }
+  },
+  methods: {
+    fetchData () {
+      return [
+        {
+          id: 1,
+          date: '28.03.2020',
+          category: 'Food',
+          value: 169
+        },
+        {
+          id: 2,
+          date: '24.03.2020',
+          category: 'Transport',
+          value: 360
+        },
+        {
+          id: 3,
+          date: '24.03.2020',
+          category: 'Food',
+          value: 532
+        },
+        {
+          id: 4,
+          date: '21.03.2020',
+          category: 'Housing',
+          value: 273
+        },
+        {
+          id: 5,
+          date: '21.03.2020',
+          category: 'Healthcare',
+          value: 545
+        },
+        {
+          id: 6,
+          date: '16.03.2020',
+          category: 'Housing',
+          value: 502
+        },
+        {
+          id: 7,
+          date: '14.03.2020',
+          category: 'Clothing',
+          value: 692
+        },
+        {
+          id: 8,
+          date: '02.03.2020',
+          category: 'Food',
+          value: 143
+        },
+        {
+          id: 9,
+          date: '01.03.2020',
+          category: 'Housing',
+          value: 339
+        }
+      ]
+    },
+    addCostsItem (data) {
+      this.paymentsList.map(i => { i.id += 1 })
+      this.paymentsList = [data, ...this.paymentsList]
+    }
+  },
+  created () {
+    this.paymentsList = this.fetchData()
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 40px;
+  margin-left: 15px;
+}
+.new-cost {
+  width: 136px;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  padding: 5px 10px;
+  color: #fff;
+  background-color: #0D98BA;
+  border: none;
+}
+</style>
---
+++ src/components/CostsList.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <ul>
+      <li class="title">
+        <span class="number">#</span>
+        <span class="date">Data</span>
+        <span class="category">Category</span>
+        <span class="value">Value</span>
+      </li>
+      <li v-for="(item, idx) in items.slice(start, end)" :key="idx">
+        <span class="number">{{ item.id }}</span>
+        <span class="date">{{ item.date }} </span>
+        <span class="category">{{ item.category }}</span>
+        <span class="value">{{ item.value }}</span>
+      </li>
+    </ul>
+    <Pagination :itemsCount="items.length" @distance="distance"/>
+  </div>
+
+</template>
+
+<script>
+
+import Pagination from './Pagination'
+export default {
+  name: 'CostsList',
+  components: {
+    Pagination
+  },
+  data () {
+    return {
+      start: 0,
+      end: 5
+    }
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    distance (data) {
+      this.start = data.start
+      this.end = data.end
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+ul {
+  width: 420px;
+  min-height: 285px;
+  list-style-type: none;
+  padding-left: 15px;
+}
+li {
+  margin-bottom: 15px;
+  padding-bottom: 15px;
+  display: flex;
+  border-bottom: 1px solid black;
+}
+
+.number, .date, .category {
+  display: block;
+  margin-right: 25px;
+}
+.date {
+  width: 100px;
+}
+.category {
+  width: 140px;
+}
+.value {
+  display: block;
+  width: 75px;
+}
+.number {
+  width: 20px;
+}
+
+</style>
