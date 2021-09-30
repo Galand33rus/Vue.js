09d2e09529e3580b68f4b9f18430b04b2b614732
Galand33rus <galand_@mail.ru>
2021-09-30 10:30:29 +0300

    lesson4.1

---
+++ src/components/CostsAdd.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="form">
+    <!--    <input placeholder="Category" v-model="category"/>-->
+    <select v-model="category">
+      <option disabled value="">Choose category</option>
+      <option
+        v-for="(option, idx) in selectList"
+        :key="idx"
+        :value="option">
+        {{ option }}
+      </option>
+    </select>
+    <input placeholder="Value" v-model="value"/>
+    <input placeholder="Date" v-model="date" type="date"/>
+    <button @click="onSaveClick">add +</button>
+    <span class="error" v-if="check">Choose category and enter a numeric value</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CostsAdd',
+  props: {
+    selectList: {
+      type: Array,
+      // default: () => [],
+      require: true
+    }
+  },
+  data () {
+    return {
+      id: 1,
+      category: '',
+      value: '',
+      date: '',
+      check: false
+    }
+  },
+  computed: {
+    categoryList () {
+      return this.getCategoryList
+    },
+    getCurrentDate () {
+      return new Intl.DateTimeFormat('ru-RU', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+      }).format(new Date())
+    }
+  },
+  methods: {
+    onSaveClick () {
+      if (this.category && !isNaN(this.value)) {
+        this.check = false
+        const data = {
+          id: this.id,
+          category: this.category,
+          value: Number(this.value),
+          date: this.date || this.getCurrentDate
+        }
+        this.$emit('addCostsItem', data)
+        this.category = ''
+        this.value = ''
+        this.date = ''
+      } else {
+        this.check = true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+select {
+  margin-top: 10px;
+  width: 173px;
+  padding: 5px 10px;
+
+  &:focus {
+    outline: none;
+  }
+}
+
+input {
+  display: block;
+  width: 150px;
+  margin-top: 10px;
+  padding: 5px 10px;
+
+  &:focus {
+    outline: none;
+  }
+}
+
+button {
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  margin-top: 10px;
+  padding: 5px 10px;
+  color: #fff;
+  background-color: #0D98BA;
+  border: none;
+}
+
+.error {
+  color: red;
+}
+</style>
